b9be3f9ea613895ae243d914684a7cb4a5ffb13a
lizhaoliu-Lec <2524463910@qq.com>
2019-05-30 23:58:13 +0800

    Check used.

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -109,6 +109,21 @@ angular.module('Controller', [])
 				return $scope.fromUserData.code && $scope.codeAgain && $scope.codeFormmatRight() && $scope.fromUserData.code == $scope.codeAgain;
 		};
 
+		$userNameUsedFormat = function () {
+			if (!'user_name' in $scope.fromUserData || !$scope.fromUserData)
+				return false;
+			else
+				return userNameUsed();
+		}
+
+		userNameUsed = function () {
+			var index = $scope.usedName.indexOf($scope.fromUserData.user_name);
+			if (index == -1)
+				return false;
+			else
+				return true;
+		}
+
 		$scope.allRight = function () {
 			var allRight = $scope.userNameValid() && codeValid() && emailValid();
 			console.log(allRight);
